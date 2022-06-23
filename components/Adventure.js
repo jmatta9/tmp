5cd1552c096bd6d633b415b0efd30ff84887c42a
Jason Matta <jt.matta@gmail.com>
2022-06-22 19:57:59 -0700

    add adventure comment

--- components/Adventure.js
+++ components/Adventure.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Comment from './Comment'
+import { FaRegCommentDots, FaRegCalendarCheck, FaFacebookSquare } from 'react-icons/fa'
 
 function Adventure() {
+  const [showComment, setShowComment] = useState(false)
   return (
-    <div className="">
-      <Comment />
+    <div>
+      {!showComment && (
+        <div className={`border-b border-[#72edfe] p-3 flex space-x-3`}>
+        <img
+          src=""//{session.user.image}
+          alt=""
+          className="h-11 w-11 cursor-pointer"
+          //onClick={signOut}
+        />
+        <div className="w-full divide-y divide-[#72edfe]">
+          <div className="w-full" />
+          <div className="flex items-center justify-between pt-2.5">
+            <div className="flex items-center space-x-3">
+              <div className="icon">
+                <FaRegCommentDots className="text-xl" onClick={()=>setShowComment(true)} />
+              </div>
+              <div className="icon">
+                <FaRegCalendarCheck className="text-xl" />
+              </div>
+              <div className="icon">
+                <FaFacebookSquare className="text-xl" />
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      )}
+      {showComment && (
+        <Comment setShowComment={setShowComment}/>
+      )}
     </div>
   )
 }
--- components/Comment.js
+++ components/Comment.js
@@ -1,19 +1,19 @@
 import React, { useState, useRef } from 'react'
-import { HiX, HiOutlinePhotograph } from 'react-icons/hi'
+import { HiX, HiOutlinePhotograph, HiOutlineEmojiHappy } from 'react-icons/hi'
+import { FaRegComment, FaRegCalendarCheck, FaFacebookSquare } from 'react-icons/fa'
 
 
-function Comment() {
+function Comment({ setShowComment }) {
   const [comment, setComment] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
-  const filePickerRef = useRef()
+  const filePickerRef = useRef(null)
+
   function handleChange(e) {
     setComment(e.target.value);
   }
+
   function handleClick(e) {
     if(e.target.id === 'x') {
-      setSelectedFile(null);
-    }
-    if(e.target.id === 'addImage') {
       setSelectedFile(null);
     }
   }
@@ -32,18 +32,17 @@ function Comment() {
         //onClick={signOut}
       />
       <div className="w-full divide-y divide-[#72edfe]">
-        <div>
           <textarea
             value={comment}
             id="comment"
             rows="2"
             placeholder="Comment..."
-            className="bg-transparent outline-none text-white text-lg tracking-wide placeholder-[#39ff14] w-full min-h-[50px]"
+            className="bg-transparent outline-none text-white text-lg tracking-wide placeholder-[#72edfe] w-full min-h-[50px] overflow-visible"
             onChange={handleChange}
           />
           {selectedFile && (
             <div className="relative">
-              <div id='x' className="absolute w-8 h-8 hover:bg-[#39ff14] bg-opacity-75 flex items-center justify-center top-1 left-1 cursor-pointer" onClick={handleClick}>
+              <div id='x' className="absolute w-8 h-8 hover:bg-[#39ff14] bg-opacity-100 flex items-center justify-center top-1 left-1 cursor-pointer" onClick={handleClick}>
               <HiX className="text-white h-5"/>
                 <img
                   src={selectedFile}
@@ -53,12 +52,24 @@ function Comment() {
               </div>
             </div>
           )}
-        </div>
+
         <div className="flex items-center justify-between pt-2.5">
-            <div className="flex items-center">
-              <div id='addImage' className="icon" onClick={handleClick}>
-                <HiOutlinePhotograph className="h-[22px] text-white" />
-                <input type="file" onChange={addImage} ref={filePickerRef} />
+            <div className="flex items-center space-x-3">
+              <div className="icon">
+                <FaRegComment className="text-xl" onClick={()=>setShowComment(false)}/>
+              </div>
+              <div id="addImage" className="icon" onClick={() => filePickerRef.current.click()}>
+                <HiOutlinePhotograph className="text-2xl" />
+                <input hidden type="file" onChange={addImage} ref={filePickerRef} />
+              </div>
+              <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+                <HiOutlineEmojiHappy className="text-2xl" />
+              </div>
+              <div className="icon">
+                <FaRegCalendarCheck className="text-xl" />
+              </div>
+              <div className="icon">
+                <FaFacebookSquare className="text-xl" />
               </div>
             </div>
         </div>
